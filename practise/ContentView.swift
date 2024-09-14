05dc70a220716556d58076a30996c8c7ae7262ab
papapahadi <singhjatin1812@gmail.com>
2024-09-14 12:18:03 +0530

    improved button ui

--- practise/ContentView.swift
+++ practise/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 class PomodoroModel : ObservableObject {
     @Published var secondsLeft = 25 * 60
+    @Published var workSession = true
     var timer : Timer?
     var timeString : String {
         let minutes = secondsLeft / 60
@@ -23,6 +24,9 @@ class PomodoroModel : ObservableObject {
             if self.secondsLeft > 0 {
                 self.secondsLeft -= 1
             }
+            else {
+                switchSession()
+            }
         }
     }
     
@@ -36,46 +40,103 @@ class PomodoroModel : ObservableObject {
         secondsLeft = workDuration * 60
     }
     
+    func switchSession() {
+        if workSession {
+            workSession = false
+        }
+        else {
+            workSession = true
+        }
+    }
+    
 }
 
 struct ContentView: View {
     @StateObject var pomodoroModel = PomodoroModel()
+    @State private var isSettingPresented = false
+
     var body: some View {
-        Pomodoro(pomodoroModel: pomodoroModel)
+        NavigationStack {
+            Pomodoro(pomodoroModel: pomodoroModel)
+                .toolbar {
+                    Button {
+                        isSettingPresented = true
+                    } label: {
+                        Image(systemName: "gear")
+                            .foregroundColor(.black)
+                    }
+                }
+                .sheet(isPresented: $isSettingPresented){
+                    Settings(pomodoroModel: pomodoroModel)
+                }
+        }
     }
 }
 
 struct Settings : View {
+    @Environment (\.dismiss) var dismissSheet
     @ObservedObject var pomodoroModel : PomodoroModel
     var body: some View {
         NavigationStack {
-            Form {
-                Section {
-                    Picker("Minutes", selection: $pomodoroModel.workDuration){
-                        ForEach(1...60, id: \.self) { minute in
-                            Text("\(minute)")
+            
+            ZStack {
+                LinearGradient(colors: [.white, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                VStack {
+                    Spacer()
+                    Section {
+                        Text("Settings")
+                            .font(.headline)
+                    }
+                    Spacer()
+                    Section(header: Text("Work Duration")) {
+                        Picker("Minutes", selection: $pomodoroModel.workDuration){
+                            ForEach(1...60, id: \.self) { minute in
+                                Text("\(minute)")
+                            }
                         }
                     }
-                }
-                Section {
-                    Picker("Break", selection: $pomodoroModel.breakDuration){
-                        
+        
+                    Section(header: Text("Break Duration")) {
+                        Picker("Break", selection: $pomodoroModel.breakDuration){
+                            ForEach(1...60, id: \.self) { second in
+                                Text("\(second)")
+                            }
+                        }
                     }
+                    
+                    Spacer()
+                    Spacer()
+                    Section {
+                        Button("save") {
+                            pomodoroModel.resetTimer()
+                            dismissSheet()
+                        }
+                        .padding()
+                        .foregroundStyle(.white)
+                        .background(LinearGradient(colors: [.red, .purple], startPoint: .top, endPoint: .bottom))
+                        .clipShape(.rect(cornerRadius: 29))
+                        .shadow(radius: 3)
+                    }
+                    Spacer()
                 }
+                
             }
-            .navigationTitle("Settings")
+                
+            
         }
     }
 }
 
 struct Pomodoro : View {
     @ObservedObject var pomodoroModel : PomodoroModel
-    @State private var pause = false
-    @State private var isSettingPresented = false
+    @State private var isPause = true
     var body: some View {
         ZStack {
             LinearGradient(colors: [.pink, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
             VStack {
+                Text(pomodoroModel.workSession ? "Work Time" : "Break TIme")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(pomodoroModel.workSession ? .red : .green)
                 
                 HStack {
                     Text(pomodoroModel.timeString)
@@ -83,33 +144,47 @@ struct Pomodoro : View {
                         .foregroundStyle(.white)
                 }
                 .padding()
-                Button{
-                    if !pause { pomodoroModel.startTimer() }
-                    else { pomodoroModel.stopTimer() }
-                    pause.toggle()
-                    
-                } label : {
-                    if !pause {
-                        Image(systemName: "play.fill")
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background(.red)
-                            .clipShape(Circle())
+                HStack {
+                    Button{
+                        if isPause { pomodoroModel.startTimer() }
+                        else { pomodoroModel.stopTimer() }
+                        isPause.toggle()
+                        
+                    } label : {
+                        if isPause {
+                            Image(systemName: "play.fill")
+                                .padding()
+                                .foregroundStyle(.white)
+                                .background(LinearGradient(colors: [.red, .purple], startPoint: .top, endPoint: .bottom))
+                                .clipShape(Circle())
+                        }
+                        else {
+                            Image(systemName: "pause.fill")
+                                .padding()
+                                .foregroundStyle(.white)
+                                .background(LinearGradient(colors: [.red, .purple], startPoint: .top, endPoint: .bottom))
+                                .clipShape(Circle())
+                                
+                        }
+                        
                     }
-                    else {
-                        Image(systemName: "pause.fill")
-                            .padding()
-                            .foregroundStyle(.white)
-                            .background(.red)
-                            .clipShape(Circle())
-                    }
+                    .padding()
                     
+                    Button {
+                        pomodoroModel.resetTimer()
+                        isPause = true
+                    } label: {
+                         Image(systemName: "arrow.clockwise")
+                            .padding()
+                            .background(LinearGradient(colors: [.blue, .purple], startPoint: .top, endPoint: .bottom))
+                            .foregroundStyle(.white)
+                            .clipShape(Circle())
+                            .shadow(radius: 5)
+                    }
+                    .padding()
                 }
     
             }
-        }
-        .toolbar {
-            Button("Settings", action: {isSettingPresented.toggle()})
         }
         .ignoresSafeArea()
     }
